2b104c092dacfdcbfe11eadb0909c052ffb57092
Mtoledo50 <marcostoledo@soluti.net.br>
2024-07-05 20:14:38 -0300

    do break line

--- src/services/products.js
+++ src/services/products.js
@@ -1,12 +1,16 @@
 // todas as funções que lidam com produtos
 // função inicial que recebe um codigo e um nome de um produto
 async function getFullName(codeId, productName){
-    console.log("\n");  // insere uma linha em branco
-    console.log("product:" + codeId + " - " + productName);
+   // console.log("\n");  // insere uma linha em branco
+   await doBreakLine();  // chamada da função para adicionar uma linha em branco
+     console.log("product:" + codeId + " - " + productName);
 }
 // fim da declaração de função de codigo e nome dos produtos
 //__________________________________
-
+// função que adiciona uma linha em branco
+async function doBreakLine(){
+    console.log("\n");
+}
 // codigo para exportação das funções.. tudo o que tiver dentro desse modulo poderá ser exportado para outro arquivo
 module.exports = {
     getFullName,
